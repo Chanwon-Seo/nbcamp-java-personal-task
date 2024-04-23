59debe97b5b71e1b0277c2dd424d8aee633385d0
Chanwon-Seo <scwonn60@gmail.com>
2024-04-23 22:32:58 +0900

    Level1-2 | feat : 사용자로부터 사칙연산 기호 입력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -2,21 +2,26 @@ package calculator;
 
 import java.util.Scanner;
 
-/** TODO
- * 1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있다.
- *      - 양의 정수는 각각 하나씩 전달 받는다.
- *      - 양의 정수는 적합한 타입으로 선언한 변수에 저장한다.
+/**
+ * TODO
+ * 2. Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.
+ *      - 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다. (`charAt(0)`)
  */
 public class App {
 
     public static void main(String[] args) {
         // Scanner 객체 생성
         Scanner sc = new Scanner(System.in);
-        // 사용자로부터 첫 번째 숫자 입력
+
+        //사용자로부터 첫 번째 숫자 입력
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int firstNum = Integer.parseInt(sc.nextLine());
-        // 사용자로부터 두 번째 숫자 입력
+        //사용자로부터 두 번째 숫자 입력
         System.out.print("두 번째 숫자를 입력하세요: ");
         int secondNum = Integer.parseInt(sc.nextLine());
+
+        //사용자로부터 사칙연산 기호 입력
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.nextLine().charAt(0);
     }
 }
