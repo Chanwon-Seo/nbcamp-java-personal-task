3c8e8f9f038328a05288dd5842da1487f0e5f05d
Chanwon-Seo <scwonn60@gmail.com>
2024-04-23 23:10:48 +0900

    Level1-3 | feat : 연산 후 결과값 출력 및 오류에 대한 정제

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 /**
  * TODO
- * 2. Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.
- *      - 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다. (`charAt(0)`)
+ * 3. 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다.
+ *      - 사칙연산 기호에 맞는 연산자를 사용하여 연산을 진행합니다.
+ *      - 입력받은 연산 기호를 구분하기 위해 제어문을 사용합니다. (e.g.if, switch)
+ *      - 연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력합니다.
+ *      - e.g. “ 나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. “
  */
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
@@ -15,13 +20,49 @@ public class App {
 
         //사용자로부터 첫 번째 숫자 입력
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        int firstNum = Integer.parseInt(sc.nextLine());
+        String firstStr = sc.nextLine();
         //사용자로부터 두 번째 숫자 입력
         System.out.print("두 번째 숫자를 입력하세요: ");
-        int secondNum = Integer.parseInt(sc.nextLine());
+        String secondStr = sc.nextLine();
+
+        //firstStr과 secondStr이 정수인지 판별
+        int firstNum, secondNum;
+        try {
+            firstNum = Integer.parseInt(firstStr);
+            secondNum = Integer.parseInt(secondStr);
+        } catch (NumberFormatException e) {
+            System.out.println("잘못된 입력입니다. 숫자를 입력하세요.");
+            return;
+        }
 
         //사용자로부터 사칙연산 기호 입력
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.nextLine().charAt(0);
+
+        //연산 기호에 따라 계산을 수행 및 결과 출력
+        try {
+            switch (operator) {
+                case '+':
+                    System.out.println(firstNum + secondNum);
+                    break;
+                case '-':
+                    System.out.println(firstNum - secondNum);
+                    break;
+                case '*':
+                    System.out.println(firstNum * secondNum);
+                    break;
+                case '/':
+                    if (secondNum == 0) {
+                        throw new IllegalArgumentException("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                    }
+                    System.out.println(firstNum / secondNum);
+                    break;
+                default:
+                    throw new IllegalArgumentException("[ +, -, /, * ] 이외에 입력되었습니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
