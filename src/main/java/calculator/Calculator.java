2f8875edcd5b38d8e26a61a3b4893cc6436c588a
Chanwon-Seo <scwonn60@gmail.com>
2024-04-24 12:57:06 +0900

    Level2-1 | feat : 사칙연산 후 결과 반환 메서드

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,28 @@
 package calculator;
 
-public class Calculator {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Calculator {
+    public Queue<Integer> opArr = new LinkedList<>();
+
+    public Calculator() {
+    }
+
+    public int calculate(int firstNum, int secondNum, char operator) {
+        int result = 0;
+        if (operator == '+') {
+            result = firstNum + secondNum;
+        } else if (operator == '-') {
+            result = firstNum - secondNum;
+        } else if (operator == '*') {
+            result = firstNum * secondNum;
+        } else if (operator == '/') {
+            if (secondNum == 0) {
+                throw new IllegalArgumentException("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+            }
+            result = firstNum / secondNum;
+        }
+        return result;
+    }
 }
