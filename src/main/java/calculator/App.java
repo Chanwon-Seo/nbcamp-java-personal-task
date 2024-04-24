ad000ac89fa908ffd609ad018f31a4353a8000ff
Chanwon-Seo <scwonn60@gmail.com>
2024-04-24 14:54:14 +0900

    Level2-2 | feat : main 연산 수행 로직 분리, 사칙연산 기호 Set 저장 및 사칙연산 입력 예외 처리

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,16 +4,26 @@ import java.util.*;
 
 /**
  * TODO
- * 8. “**inquiry”라는 문자열이 입력되면 저장된 연산 결과 전부를 출력합니다.**
- *      - foreach(향상된 for문)을 활용하여 구현 해봅니다.
+ * 2. Level 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정합니다.
+ *  - 연산 수행 역할은 Calculator 클래스가 담당합니다.
+ *      - 연산 결과는 Calculator 클래스의 연산 결과를 저장하는 필드에 저장됩니다.
+ *  - 소스 코드 수정 후에도 수정 전의 기능들이 반드시 똑같이 동작해야합니다.
  */
 public class App {
+    //사칙연산에 필요한 기호 Set
+    private static final Set<Character> operatorSet = new HashSet<>();
+    static {
+        operatorSet.add('+');
+        operatorSet.add('-');
+        operatorSet.add('*');
+        operatorSet.add('/');
+    }
 
     public static void main(String[] args) {
         // Scanner 객체 생성
         Scanner sc = new Scanner(System.in);
-        //연산 결과를 저장 v1
-        Queue<Integer> opArr = new LinkedList<>();
+        //Calculator 생성
+        Calculator calculator = new Calculator();
 
         String flag = "";
         while (!flag.equals("exit")) {
@@ -36,44 +46,49 @@ public class App {
 
             //사용자로부터 사칙연산 기호 입력
             System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.nextLine().charAt(0);
-            int result = 0;
-            //연산 기호에 따라 계산을 수행
+            char operator;
             try {
-                result = switch (operator) {
-                    case '+' -> firstNum + secondNum;
-                    case '-' -> firstNum - secondNum;
-                    case '*' -> firstNum * secondNum;
-                    case '/' -> {
-                        if (secondNum == 0) {
-                            throw new IllegalArgumentException("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                        }
-                        yield firstNum / secondNum;
-                    }
-                    default -> throw new IllegalArgumentException("[ +, -, /, * ] 이외에 입력되었습니다.");
-                };
-            } catch (IllegalArgumentException e) {
+                operator = sc.nextLine().charAt(0);
+                if (!operatorSet.contains(operator)) {
+                    throw new InputMismatchException("[ +, -, /, * ] 이외에 입력되었습니다.");
+                }
+            } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
+                continue;
             }
 
-            opArr.add(result);
-            System.out.println("결과 = " + opArr.peek() + "\n");
+            //사칙연산 후 calculator.opArr에 "직접 호출" 후 저장
+            try {
+                int result = calculator.calculate(firstNum, secondNum, operator);
+                calculator.opArr.add(result);
+                System.out.println("결과 = " + calculator.opArr.peek() + "\n");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-
             if (sc.nextLine().equals("remove")) {
-                if (!opArr.isEmpty()) {
-                    System.out.println("remove = " + opArr.poll());
+                if (!calculator.opArr.isEmpty()) {
+                    System.out.println("remove = " + calculator.opArr.poll());
+                } else {
+                    System.out.println("삭제할 연산 결과가 없습니다.");
                 }
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             if (sc.nextLine().equals("inquiry")) {
                 int index = 1;
-                for (Integer i : opArr) {
-                    System.out.println(index + "번째 결과는 = " + i);
-                    index++;
+                if (!calculator.opArr.isEmpty()) {
+                    for (Integer i : calculator.opArr) {
+                        System.out.println(index + "번째 결과는 = " + i);
+                        index++;
+                    }
+                } else {
+                    System.out.println("조회할 연산 결과가 없습니다.");
                 }
+
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
